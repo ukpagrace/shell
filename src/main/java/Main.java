bc88284d5cf6c2c3e50471a129d5f286192e5b46
Ukpa Grace <ukpagraceu@gmail.com>
2024-12-05 11:03:06 +0100

    added command not found for invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        System.out.print(input + ": command not found");
     }
 }
