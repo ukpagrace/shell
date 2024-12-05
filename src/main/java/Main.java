d64200a83e77f67b25a85d52b9e5f28e3e450536
Ukpa Grace <ukpagraceu@gmail.com>
2024-12-05 18:05:04 +0100

    added type to the repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,11 +10,21 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
+            ArrayList<String> commands = new ArrayList<>();
 
-            if(input.contains("exit 0")){
+            if(input.equals("exit 0")){
                     break;
-            }
-            else if(input.contains("echo")){
+            }else if(input.startsWith("type")){
+                commands.add("exit");
+                commands.add("echo");
+                String command = input.substring(5);
+                if(commands.contains(command)){
+                    System.out.print(input.substring(5) + " is a shell builtin \n");
+                }else{
+                    System.out.print(input.substring(5) + ": command not found\n");
+                }
+
+            }else if(input.startsWith("echo")){
                 System.out.print(input.substring(5) + "\n");
             }else{
                 System.out.print(input + ": command not found\n");
