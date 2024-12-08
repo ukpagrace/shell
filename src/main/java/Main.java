4c18ceedce2bfceb30d09c4be6813aefe6b02879
Ukpa Grace <ukpagraceu@gmail.com>
2024-12-08 17:16:50 +0100

    debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -46,7 +46,9 @@ public class Main {
                 matchList.addFirst("-c");
                 matchList.addFirst("sh");
 //                String[] mand = {"sh", "-c", "echo \"This is inline text\" > myfile.txt && cat myfile.txt"};
-                Process process = Runtime.getRuntime().exec(matchList.toArray(new String[0]));
+                String[] stringArray = new String[matchList.size()];
+                stringArray = matchList.toArray(stringArray);
+                Process process = Runtime.getRuntime().exec(stringArray);
                 process.getInputStream().transferTo(System.out);
 
 //                System.out.println("matchlist to array" + Arrays.toString(matchList.toArray(new String[0])));
