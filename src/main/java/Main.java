37edf93295c71b30a9f47f3545bc4c54424c6f8a
Ukpa Grace <ukpagraceu@gmail.com>
2024-12-08 12:20:01 +0100

    debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,6 +43,8 @@ public class Main {
             }
 
             if(!commands.contains(command) && getPath(command) != null){
+                matchList.addFirst("-c");
+                matchList.addFirst("bash");
                 System.out.println("matchlist to array" + Arrays.toString(matchList.toArray(new String[0])));
                 Process process = Runtime.getRuntime().exec(matchList.toArray(new String[0]));
                 process.getInputStream().transferTo(System.out);
