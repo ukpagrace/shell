e57c09727fdb965ceae1f28f419b256f7ab6d640
Ukpa Grace <ukpagraceu@gmail.com>
2024-12-08 01:04:10 +0100

    refactored code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -71,13 +71,7 @@ public class Main {
                         System.out.println(parameter);
                         break;
                     case("pwd"):
-//                        String userDirectory = Paths.get("").toAbsolutePath().toString();
                         System.out.println(System.getProperty("user.dir"));
-//                        System.out.println(userDirectory);
-
-//                        System.out.println("hello");
-
-
                         break;
                     case("cd"):
 
@@ -86,24 +80,11 @@ public class Main {
 
                         for (String s : paramArray) {
                             Path path = currentWorkingDir.resolve(s).toAbsolutePath().normalize();
-//                            System.out.println("path" +  path);
-
                             if (Files.isDirectory(Path.of(path.toString()))) {
                                 System.setProperty("user.dir", path.toString());
-//                            System.out.println("absolute " + Paths.get("").toAbsolutePath().toString());
-//                            System.out.println("system "+ System.getProperty("user.dir"));
                             } else {
                                 System.out.println(command + ": " + parameter + ": No such file or directory");
                             }
-//                            if(paramArray[i].startsWith("../")){
-//
-//                            }else if(paramArray[i].startsWith("./") && Files.isDirectory(Path.of(parameter))){
-//                                System.setProperty("user.dir", Path.of(paramArray[i].substring(2)).getParent().toString());
-//                            }else if(paramArray[i].startsWith("/")){
-//                                System.setProperty("user.dir", new File(parameter).getAbsolutePath());
-//                            }else{
-//
-//                            }
                         }
 
                         break;
